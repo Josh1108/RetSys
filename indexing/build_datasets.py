c5cd72c4beb25df742fa7800713218081c9b5d55
jushaan <Jushaan18@gmail.com>
2025-01-31 20:41:18 -0500

    refactor: changed functionality of build and index

--- indexing/build_datasets.py
+++ indexing/build_datasets.py
@@ -1,31 +1,86 @@
 import os
 import json
-import argparse
 from datasets import Dataset
+import PyPDF2
 
+class DatasetConverter:
+    def __init__(self):
+        """
+        Initialize the DatasetConverter with an empty list to store data.
+        """
+        self.data_list =[]
 
-def push_dataset_to_hub(data_dir, dataset_name, private=False):
-    # Directory containing JSON files
-    data_dir = "/home/jkalra/directed_study/current_databases/docs/jsons"
+    def insert_json_file(self, json_file_path:str):
+        """
+        Insert a JSON file into the dataset.
 
-    # List all JSON files in the directory
-    json_files = [f for f in os.listdir(data_dir) if f.endswith(".json")]
-
-    data = []
-    for file_name in json_files:
-        file_path = os.path.join(data_dir, file_name)
-        with open(file_path, "r") as f:
+        :param json_file_path: Path to the JSON file to be inserted.
+        :type json_file_path: str
+        """
+        with open(json_file_path, "r") as f:
             json_data = json.load(f)  # Load JSON content
             combined_text = " ".join(json_data)  # Combine the list of texts
-            data.append({"file_name": file_name, "document": combined_text})
+            self.data_list.append({"file_name": json_file_path, "document": combined_text})
+    
+    def parse_pdf_file(self, pdf_file_path:str):
+        """
+        Parse a PDF file and insert its content into the dataset.
 
-    processed_dataset = Dataset.from_list(data, split="full")
+        :param pdf_file_path: Path to the PDF file to be parsed.
+        :type pdf_file_path: str
+        """
+        with open(pdf_file_path, "rb") as f:
+            pdf_reader = PyPDF2.PdfReader(f)
+            for page in pdf_reader.pages:
+                text = page.extract_text()
+                self.data_list.append({"file_name": pdf_file_path, "document": text})
+    def load_dir_files(self, data_dir:str):
+        """
+        Load all files in a directory and insert them into the dataset.
 
-    processed_dataset.push_to_hub(dataset_name, private=True)
+        :param data_dir: Path to the directory containing files to be loaded.
+        :type data_dir: str
+        """
+        for file in os.listdir(data_dir):
+            if file.endswith(".json"):
+                self.insert_json_file(os.path.join(data_dir, file))
+            elif file.endswith(".pdf"):
+                self.parse_pdf_file(os.path.join(data_dir, file))
 
+    def save_dataset(self, dataset_name:str, private:bool=False,save_locally:bool=False,save_on_hf_hub:bool=False):
+        """
+        Save the dataset to a local file or push it to the Hugging Face Hub.
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--data_dir", required=True)
-parser.add_argument("--index_name", required=True)
-args = parser.parse_args()
-push_dataset_to_hub(args.data_dir, args.index_name)
+        :param dataset_name: Name of the dataset to be saved or pushed.
+        :type dataset_name: str
+        :param private: Whether the dataset should be private.
+        :type private: bool
+        :param save_locally: Whether to save the dataset locally.
+        :type save_locally: bool
+        :param save_on_hf_hub: Whether to push the dataset to the Hugging Face Hub.
+        :type save_on_hf_hub: bool
+        """
+        assert save_locally or save_on_hf_hub, "Must save dataset locally or on HF Hub"
+        processed_dataset = Dataset.from_list(self.data_list, split="full")
+        if save_locally:
+            processed_dataset.save_to_disk(dataset_name)
+        elif save_on_hf_hub:
+            processed_dataset.push_to_hub(dataset_name, private=private)
+    
+    def run(self, data_dir:str, dataset_name:str, private:bool=False,save_locally:bool=False,save_on_hf_hub:bool=False):
+        """
+        Run the dataset conversion and saving process.
+
+        :param data_dir: Path to the directory containing files to be loaded.
+        :type data_dir: str
+        :param dataset_name: Name of the dataset to be saved or pushed.
+        :type dataset_name: str
+        :param private: Whether the dataset should be private.
+        :type private: bool
+        :param save_locally: Whether to save the dataset locally.
+        :type save_locally: bool
+        :param save_on_hf_hub: Whether to push the dataset to the Hugging Face Hub.
+        :type save_on_hf_hub: bool
+        """
+        self.load_dir_files(data_dir)
+        self.save_dataset(dataset_name, private, save_locally, save_on_hf_hub)
--- indexing/build_index.py
+++ indexing/build_index.py
@@ -6,75 +6,103 @@ from utils import utils
 from indexing.kv_store import KVStore
 
 
-def get_index_name(args: argparse.Namespace) -> str:
-    return os.path.basename(args.dataset_path) + "." + args.key
+class IndexBuilder:
+    def __init__(self, index_type: str, index_name: str, save_dir: str, granularity: str = "null"):
+        """
+        Initialize the IndexBuilder class.
 
+        :param index_type: The type of index to build.
+        :type index_type: str
+        :param index_name: The name of the index.
+        :type index_name: str
+        :param save_dir: The directory to save the index.
+        :type save_dir: str
+        :param granularity: The granularity of the index.
+        :type granularity: str, defaults to "null"
+        :type granularity: str, optional
+        """
+        self.index_type = index_type
+        self.index_name = index_name
+        self.granularity = granularity
+        self.save_dir = save_dir
+        self.index = self.initialize_index()
+    def initialize_index(self) -> KVStore:
+        """
+        Initialize the index.
 
-def create_index(args: argparse.Namespace) -> KVStore:
-    index_name = get_index_name(args)
+        :raises ValueError: If the index type is not valid.
+        :raises ValueError: If the granularity is not valid.
+        :return: The index.
+        :rtype: KVStore
+        """
+        if self.index_type == "bm25":
+            from indexing.bm25 import BM25
 
-    if args.index_type == "bm25":
-        from indexing.bm25 import BM25
+            index = BM25(self.index_name)
+        elif self.index_type == "instructor":
+            from indexing.instructor import Instructor
 
-        index = BM25(index_name)
-    elif args.index_type == "instructor":
-        from indexing.instructor import Instructor
+            if self.granularity == "propositions":
+                query_instruction = "Represent the question for retrieving propositions from relevant documents:"
+                key_instruction = "Represent the propositions of a document for retrieval:"
+            elif self.granularity == "paragraphs":
+                query_instruction = "Represent the question for retrieving passages from relevant documents:"
+                key_instruction = "Represent the passage from the documents for retrieval:"
+            else:
+                raise ValueError("Invalid granularity, must be 'propositions' or 'paragraphs'")
+            index = Instructor(self.index_name, key_instruction, query_instruction)
+        elif self.index_type == "e5":
+            from indexing.e5 import E5
 
-        if args.key == "propositions":
-            query_instruction = "Represent the question for retrieving propositions from relevant documents:"
-            key_instruction = "Represent the propositions of a document for retrieval:"
-        elif args.key == "paragraphs":
-            query_instruction = "Represent the question for retrieving passages from relevant documents:"
-            key_instruction = "Represent the passage from the documents for retrieval:"
+            index = E5(self.index_name)
+        elif self.index_type == "gtr":
+            from indexing.gtr import GTR
+
+            index = GTR(self.index_name)
         else:
-            raise ValueError("Invalid key")
-        index = Instructor(index_name, key_instruction, query_instruction)
-    elif args.index_type == "e5":
-        from indexing.e5 import E5
+            raise ValueError("Invalid index type")
+        return index
+    
+    def create_kv_pairs(self, data: List[dict]) -> dict:
+        """
+        Create key-value pairs for the index.
 
-        index = E5(index_name)
-    elif args.index_type == "gtr":
-        from indexing.gtr import GTR
-
-        index = GTR(index_name)
-    else:
-        raise ValueError("Invalid index type")
-    return index
-
-
-def create_kv_pairs(data: List[dict], key: str) -> dict:
-    if key == "propositions":
-        kv_pairs = {}
-        propositions = utils.get_clean_propositions(data)  # load libraries once
-        for i, record in enumerate(data):
-            corpusid = utils.get_clean_corpusid(record)
-            for proposition_idx, proposition in enumerate(propositions[i]):
-                kv_pairs[proposition] = (corpusid, proposition_idx)
-    elif key == "paragraphs":
-        kv_pairs = {}
-        for record in data:
-            corpusid = utils.get_clean_corpusid(record)
-            paragraphs = utils.get_clean_paragraphs(record)
-            for paragraph_idx, paragraph in enumerate(paragraphs):
-                kv_pairs[paragraph] = (corpusid, paragraph_idx)
-    else:
-        raise ValueError("Invalid key")
-    return kv_pairs
+        :param data: The data to create key-value pairs from.
+        :type data: List[dict]
+        :return: The key-value pairs.
+        :rtype: dict
+        """
+        if self.granularity == "propositions":
+            kv_pairs = {}
+            propositions = utils.get_clean_propositions(data)  # load libraries once
+            for i, record in enumerate(data):
+                corpusid = utils.get_clean_corpusid(record)
+                for proposition_idx, proposition in enumerate(propositions[i]):
+                    kv_pairs[proposition] = (corpusid, proposition_idx)
+        elif self.granularity == "paragraphs":
+            kv_pairs = {}
+            for record in data:
+                corpusid = utils.get_clean_corpusid(record)
+                paragraphs = utils.get_clean_paragraphs(record)
+                for paragraph_idx, paragraph in enumerate(paragraphs):
+                    kv_pairs[paragraph] = (corpusid, paragraph_idx)
+        return kv_pairs
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--index_type", required=True
-)  # bm25, instructor, dpr, e5, gtr, grit
-parser.add_argument("--key", required=True)  # paragraphs, propositions
-parser.add_argument("--dataset_path", required=False, default="Jo1811/maritime_docs")
-parser.add_argument("--index_root_dir", required=False, default="retrieval_indices")
-args = parser.parse_args()
 
-corpus_data = datasets.load_dataset(args.dataset_path, split="full")
-index = create_index(args)
-kv_pairs = create_kv_pairs(corpus_data, args.key)
-index.create_index(kv_pairs)
+# parser = argparse.ArgumentParser()
+# parser.add_argument(
+#     "--index_type", required=True
+# )  # bm25, instructor, dpr, e5, gtr, grit
+# parser.add_argument("--key", required=True)  # paragraphs, propositions
+# parser.add_argument("--dataset_path", required=False, default="Jo1811/maritime_docs")
+# parser.add_argument("--index_root_dir", required=False, default="retrieval_indices")
+# args = parser.parse_args()
 
-index_name = get_index_name(args)
-index.save(args.index_root_dir)
+# corpus_data = datasets.load_dataset(args.dataset_path, split="full")
+# index = initialize_index(args.index_type, args.index_name, args.key)
+# kv_pairs = create_kv_pairs(corpus_data, args.key)
+# index.create_index(kv_pairs)
+
+# index_name = get_index_name(args)
+# index.save(args.index_root_dir)
